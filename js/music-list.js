85c776ec59bd6a708d6bbdf79cc1cfffd063e7f3
Haruto Kairi <chieunguyentat@gmail.com>
2025-08-11 03:16:35 +0700

    Update music-list.js

--- js/music-list.js
+++ js/music-list.js
@@ -1,18 +1,21 @@
 let allMusic = [
   {
     name: "Magnetic",
+    separator: " - ",
     artist: "ILLIT",
     img: "music-1",
     src: "music-1"
   },
   {
     name: "Lucky Girl Syndrome",
+     separator: " - ",
     artist: "ILLIT",
     img: "music-2",
     src: "music-2"
   },
   {
     name: "Midnight Fiction",
+        separator: " - ",
     artist: "ILLIT",
     img: "music-3",
     src: "music-3"
