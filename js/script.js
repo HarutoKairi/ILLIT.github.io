153af1391faa6eab28dded169b02a9916ed1cef1
Haruto Kairi <chieunguyentat@gmail.com>
2025-08-11 04:10:43 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -180,7 +180,7 @@ for (let i = 0; i < allMusic.length; i++) {
                 <div class="row">
                   <span>${allMusic[i].name}</span>
 <span> - </span>
-                  <p>${allMusic[i].artist}</p>
+                  <p>  ${allMusic[i].artist}</p>
                 </div>
                 <span id="${allMusic[i].src}" class="audio-duration">3:40</span>
                 <audio class="${allMusic[i].src}" src="songs/${allMusic[i].src}.mp3"></audio>
