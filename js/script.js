b587335d07b690b66661fa39ce5bde6134c3d4d8
Haruto Kairi <chieunguyentat@gmail.com>
2025-08-11 03:23:02 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -179,6 +179,9 @@ for (let i = 0; i < allMusic.length; i++) {
   let liTag = `<li li-index="${i + 1}">
                 <div class="row">
                   <span>${allMusic[i].name}</span>
+
+                            <span class="separator">${music.separator}</span>
+                            
                   <p>${allMusic[i].artist}</p>
                 </div>
                 <span id="${allMusic[i].src}" class="audio-duration">3:40</span>
